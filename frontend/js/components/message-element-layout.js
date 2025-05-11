d1486fed6736438edd3cb8e095b3ff74d71f2131
sasha1985y <kuyancev1985@mail.ru>
2025-05-11 19:38:24 +0300

    добавил функцию для отрисовки сообщения

---
+++ frontend/js/components/message-element-layout.js
@@ -0,0 +1,11 @@
+export function createMessageElement(message) {
+    return (`
+                <div class="message-popup-container"></div>
+                <div class="message-panel">
+                    <div class="message-author">${message.username}</div>
+                    <button class="message-control"></button>
+                </div>
+                <p class="message-text">${message.text}</p>
+                <time>${message.timestamp}</time>
+            `)
+}
