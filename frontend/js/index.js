08ba7870a8d918e75867bc7199f49982cc5d630a
Alexander <77992353+sasha1985y@users.noreply.github.com>
2025-05-11 21:30:18 +0300

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,3 @@
-
 import {
     createMessageElement,
     createMessagePopup,
@@ -21,49 +20,6 @@ headerChatLable.innerHTML = "";
 
 function renderMessages(messages, container) {
     let index = 0;
-
-/**
- * Требования:
- * - Прозрачная обратная связь — в любой момент времени пользователь
- *   должен понимать что происходит с интерфейсомы
- *   - Можно ли писать текст сообщения?
- *   - Валидно ли сообщение, которое он отправляет и можно ли его отправить?
- *   - После отправки 
- *    - началась ли отправка?
- *    - пришло ли сообщение на сервер? удачно ли?
- *    - [отображение сообщения в списке]
- * 
- * 1. Я нажал на кнопку отправить
- * 2. На сервер ушел POST-запрос
- * 3. Сервер обработал этот запрос
- * 4. Вернул мне ответ
- * 5. Я обработал ответ, понял есть ли ошибка
- * 6. Если нет ошибки — показал это
- * 6.1 Если есть ошибка — показал это
- * 
- * Хорошо бы дать возможность пользователю не отправлять одно и то же сообщение
- * несколько раз
- * 
- * Способы обратной связи 
- * 1. Ничего не делать
- * 2. Все заблокировать
- *   1. Заблокировать поле ввода и кнопку и поменять текст на кнопке
- *   2. Если удачно — разблокировать и вернуть текст обратно, очистить форму и отобразить обновленный список сообщений
- *   3. Если ошибка — разблокировать и вернуть текст обратно, не сбрасывать форму и показать ошибку
- * 3. Optimistic UI
- *   1. Мгновенно обновляет список сообщений и показывает наше сообщение в списке
- *      Очищает форму и дает возможность отправить новое сообщение
- *      Вновь созданному сообщению добавляет визуальный индикатор о его состоянии
- */
-
-
-{
-  const container = document.querySelector(".messages");
-
-  function renderMessages(messages) {
-    container.innerHTML = "";
-
-
     for (const message of messages) {
         const messageElement = document.createElement("article");
         messageElement.className = "message";
@@ -72,30 +28,15 @@ function renderMessages(messages, container) {
             messageElement.classList.add("odd-numbered");
         }
 
-
         messageElement.innerHTML = createMessageElement(message);
-
-      messageElement.innerHTML = `
-        <div class="message-header">
-          <div class="message-author">${message.username}</div>
-          <button class="message-control"></button>
-        </div>
-        <p class="message-text">${message.text}</p>
-        <time class="message-time">${message.timestamp}</time>
-      `;
-
 
         container.appendChild(messageElement);
         index++;
     }
 }
 
-
 function getMessages(container) {
     const formSubmitInfo = document.querySelector(".form-submit-info");
-
-  function getMessages() {
-
     fetch("http://localhost:4000/messages", {
         method: "GET",
     })
@@ -289,7 +230,6 @@ function toggleBtnLogic() {
 
     } else {
 
-
         showElement(template1, main);
 
         mode = "greeting";
@@ -301,57 +241,6 @@ function toggleBtnLogic() {
         toggleButton.addEventListener("click", toggleBtnLogic);
 
     }
-
-  function initForm() {
-    const formContainer = document.querySelector("form");
-
-    const formTextField = formContainer.querySelector("textarea");
-    const formSubmitButton = formContainer.querySelector("button");
-
-    formContainer.onsubmit = function(evt) {
-      evt.preventDefault();
-      const formData = new FormData(evt.target);
-
-      const messageData = {
-        username: formData.get("username"),
-        text: formData.get("text"),
-      };
-
-      formTextField.disabled = true;
-      formSubmitButton.disabled = true;
-      formSubmitButton.textContent = "Сообщение отправляется...";
-
-      fetch("http://localhost:4000/messages", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(messageData),
-      })
-        .then(function(newMessageResponse) {
-          console.log(newMessageResponse.status);
-
-          if (newMessageResponse.status !== 200) {
-            //
-          }
-
-          formTextField.disabled = false;
-          formTextField.value = "";
-          formSubmitButton.disabled = false;
-          formSubmitButton.textContent = "Отправить";
-
-          getMessages();
-        });
-    }
-  }
-
-  function initChat() {
-    getMessages();
-    initForm();
-  }
-
-  initChat();
-
 }
 
 toggleButton.addEventListener("click", toggleBtnLogic);
