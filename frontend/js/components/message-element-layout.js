3c72c34eabbc8ab6a5b6e757af67e50b7588aff8
sasha1985y <kuyancev1985@mail.ru>
2025-05-24 21:09:47 +0300

    добавил класс, чтобы можно было удалять массово ненужные элементы

--- frontend/js/components/message-element-layout.js
+++ frontend/js/components/message-element-layout.js
@@ -14,12 +14,12 @@
 
 export function createMessageElement(message) {
     return (`
-                <div class="message-popup-container"></div>
-                <div class="message-panel">
-                    <div class="message-author">${message.username}</div>
-                    <button class="message-control"></button>
+                <div class="message-popup-container removable"></div>
+                <div class="message-panel removable">
+                    <div class="message-author removable">${message.username}</div>
+                    <button class="message-control removable"></button>
                 </div>
                 <p class="message-text">${message.text}</p>
-                <time>${message.timestamp}</time>
+                <time class="removable">${message.timestamp}</time>
             `)
 }
