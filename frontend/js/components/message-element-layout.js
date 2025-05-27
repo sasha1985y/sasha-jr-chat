a1686d730dc1ea6230cffae87550409451f09997
sasha1985y <kuyancev1985@mail.ru>
2025-05-28 02:53:28 +0300

    убрал класс, который служит сигналомдля удоаления некоторых элементов, чтобы не было ошибок при рендере

--- frontend/js/components/message-element-layout.js
+++ frontend/js/components/message-element-layout.js
@@ -4,7 +4,7 @@
                             <div class="message-popup-container"></div>
                             <div class="message-panel">
                                 <div class="message-author">${message.username}</div>
-                                <div class="message-delete removable">${message.lifetime}</div>
+                                <div class="message-delete">${message.lifetime}</div>
                                 <button class="message-control"></button>
                             </div>
                             <p class="message-text">${message.text}</p>
@@ -16,9 +16,9 @@
 export function createMessageElement(message) {
     return (`
                 <div class="message-popup-container removable"></div>
-                <div class="message-panel removable">
+                <div class="message-panel">
                     <div class="message-author removable">${message.username}</div>
-                    <div class="message-delete removable">${message.lifetime}</div>
+                    <div class="message-delete">${message.lifetime}</div>
                     <button class="message-control removable"></button>
                 </div>
                 <p class="message-text">${message.text}</p>
