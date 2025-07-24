bbdde0a33afe1e9daf8496f294e422973d5fa098
sasha1985y <kuyancev1985@mail.ru>
2025-07-25 01:51:26 +0300

    add process, client, serverInit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dayjs from "dayjs";
 
+import { Client } from "pg";
+
+const PORT = process.env.APP_PORT || 4000;
+
 type Message = {
     "id": number,
     "username": string,
@@ -10,8 +14,17 @@ type Message = {
     "lifetime": number,
 };
 
+// const client = new Client();
+const client = new Client({
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: parseInt(process.env.PGPORT || "5432", 10),
+    database: process.env.PGDATABASE,
+});
+
 const server = express();
-const PORT = 4000;
+//const PORT = 4000;
 
 const messages: Message[] = [];
 
@@ -22,198 +35,219 @@ function* infiniteSequence() {
     }
 }
 
-const idIterator = infiniteSequence();
+async function initServer() {
+    if (!process.env.PGUSER) {
+        throw new Error("Server cannot be started without database credentials provided in .env file");
+    }
 
-server.use(cors());
-server.use(express.json());
-
-// Функция, которая добавляет системное сообщение
-function addMidnightMessage() {
-    const currentTimeStr = dayjs().format("D MMMM YYYY");
-    const timeMessage: Message = {
-        id: idIterator.next().value as number,
-        username: "System",
-        text: `<div>${currentTimeStr}</div>`,
-        timestamp: dayjs().toISOString(),
-        lifetime: 86400,
-    };
+    const idIterator = infiniteSequence();
     
-    messages.push(timeMessage);
-
-    const timerInterval = setInterval(() => {
-        timeMessage.lifetime--;
-
-        if (timeMessage.lifetime <= 0) {
-            const systemMessageIndex = messages.findIndex(message => message.username === "System");
-            if (systemMessageIndex !== -1) {
-                messages.splice(systemMessageIndex, 1); // Удаляем сообщение по индексу
+    server.use(cors());
+    server.use(express.json());
+    
+    // Функция, которая добавляет системное сообщение
+    function addMidnightMessage() {
+        const currentTimeStr = dayjs().format("D MMMM YYYY");
+        const timeMessage: Message = {
+            id: idIterator.next().value as number,
+            username: "System",
+            text: `<div>${currentTimeStr}</div>`,
+            timestamp: dayjs().toISOString(),
+            lifetime: 86400,
+        };
+        
+        messages.push(timeMessage);
+    
+        const timerInterval = setInterval(() => {
+            timeMessage.lifetime--;
+    
+            if (timeMessage.lifetime <= 0) {
+                const systemMessageIndex = messages.findIndex(message => message.username === "System");
+                if (systemMessageIndex !== -1) {
+                    messages.splice(systemMessageIndex, 1); // Удаляем сообщение по индексу
+                }
+    
+                clearInterval(timerInterval);
             }
-
-            clearInterval(timerInterval);
-        }
-    }, 1000);
-
-}
-
-setInterval(checkMidnight, 1000);
-
-function checkMidnight() {
-  const now = dayjs();
-  if (now.hour() === 0 && now.minute() === 0 && now.second() === 0) {
-    addMidnightMessage();
-  }
-}
-
-function addMessageWithLifetime(message: Message) {
-    messages.push(message);
-
-    setTimeout(() => {
-        const messageIndex = messages.findIndex(m => m.id === message.id);
-        if (messageIndex !== -1) {
-            messages.splice(messageIndex, 1);
-        }
-    }, message.lifetime * 1000);
-
-    setInterval(() => {
-        message.lifetime--;
-    }, 1000);
-}
-
-server.get("/messages", function (req: Request, res: Response) {
-    res.status(200).json([...messages]);
-});
-
-
-server.post("/messages", function (req: Request, res: Response) {
-    const { username, text } = req.body;
-
-    if (typeof username !== "string") {
-        res.status(400).send({
-            message: "Username must be a string",
-        });
-        return;
-    }
-
-    if (username.length < 2) {
-        res.status(400).send({
-            message: "Username must be at least 2 characters long",
-        });
-        return;
-    }
-
-    if (username.length > 20) {
-        res.status(400).send({
-            message: "Username must be no more than 20 characters long",
-        });
-        return;
-    }
-
-    if (typeof text !== "string") {
-        res.status(400).send({
-            message: "Message text must be a string",
-        });
-        return;
-    }
-
-    if (text.length < 1) {
-        res.status(400).send({
-            message: "Message text must be at least 1 character long",
-        });
-        return;
-    }
-
-    if (text.length > 500) {
-        res.status(400).send({
-            message: "Message text must be no more than 500 characters long",
-        });
-        return;
-    }
-
-    const newMessage = {
-        id: idIterator.next().value as number,
-        text,
-        timestamp: dayjs(new Date().toISOString()).format("HH:mm"),
-        username,
-        lifetime: 60,
-    };
+        }, 1000);
     
-    //messages.push(newMessage);
-    addMessageWithLifetime(newMessage);
-    res.status(201).send(newMessage);
-});
-
-server.patch("/messages/:id", function (req: Request, res: Response) {
-    const messageId = parseInt(req.params.id, 10);
-    const { text, username } = req.body;
-
-    const messageIndex = messages.findIndex(message => message.id === messageId);
+    }
     
-    if (messageIndex === -1) {
-        res.status(404).send({
-            message: "Message not found",
-        });
-        return;
+    setInterval(checkMidnight, 1000);
+    
+    function checkMidnight() {
+      const now = dayjs();
+      if (now.hour() === 0 && now.minute() === 0 && now.second() === 0) {
+        addMidnightMessage();
+      }
     }
-
-    if (messages[messageIndex].username.trim().replaceAll(" ", "") !== username.trim().replaceAll(" ", ""))  {
-        res.status(403).send({
-            message: "You can only edit your own messages",
-        });
-        return;
+    
+    function addMessageWithLifetime(message: Message) {
+        messages.push(message);
+    
+        setTimeout(() => {
+            const messageIndex = messages.findIndex(m => m.id === message.id);
+            if (messageIndex !== -1) {
+                messages.splice(messageIndex, 1);
+            }
+        }, message.lifetime * 1000);
+    
+        setInterval(() => {
+            message.lifetime--;
+        }, 1000);
     }
-
-    if (typeof text !== "string") {
-        res.status(400).send({
-            message: "Message text must be a string",
-        });
-        return;
-    }
-
-    if (text.length < 1) {
-        res.status(400).send({
-            message: "Message text must be at least 1 character long",
-        });
-        return;
-    }
-
-    if (text.length > 500) {
-        res.status(400).send({
-            message: "Message text must be no more than 500 characters long",
-        });
-        return;
-    }
-
-    messages[messageIndex].text = text;
-    res.status(200).send(messages[messageIndex]);
-});
-
-server.delete("/messages/:id", function (req: Request, res: Response) {
-    const messageId = parseInt(req.params.id, 10);
-    const { username } = req.body;
-
-    const messageIndex = messages.findIndex(message => message.id === messageId);
-
-    if (messageIndex === -1) {
-        res.status(404).send({
-            message: "Message not found",
-        });
-        return;
-    }
-
-    if (messages[messageIndex].username.trim().replaceAll(" ", "") !== username.trim().replaceAll(" ", "")) {
-        res.status(403).send({
-            message: "You can only delete your own messages",
-        });
-        return;
-    }
-
-    messages.splice(messageIndex, 1);
-    res.status(200).send({
-        message: "Message deleted successfully",
+    
+    server.get("/messages", function (req: Request, res: Response) {
+        res.status(200).json([...messages]);
     });
+    
+    
+    server.post("/messages", function (req: Request, res: Response) {
+        const { username, text } = req.body;
+    
+        if (typeof username !== "string") {
+            res.status(400).send({
+                message: "Username must be a string",
+            });
+            return;
+        }
+    
+        if (username.length < 2) {
+            res.status(400).send({
+                message: "Username must be at least 2 characters long",
+            });
+            return;
+        }
+    
+        if (username.length > 20) {
+            res.status(400).send({
+                message: "Username must be no more than 20 characters long",
+            });
+            return;
+        }
+    
+        if (typeof text !== "string") {
+            res.status(400).send({
+                message: "Message text must be a string",
+            });
+            return;
+        }
+    
+        if (text.length < 1) {
+            res.status(400).send({
+                message: "Message text must be at least 1 character long",
+            });
+            return;
+        }
+    
+        if (text.length > 500) {
+            res.status(400).send({
+                message: "Message text must be no more than 500 characters long",
+            });
+            return;
+        }
+    
+        const newMessage = {
+            id: idIterator.next().value as number,
+            text,
+            timestamp: dayjs(new Date().toISOString()).format("HH:mm"),
+            username,
+            lifetime: 60,
+        };
+        
+        //messages.push(newMessage);
+        addMessageWithLifetime(newMessage);
+        res.status(201).send(newMessage);
+    });
+    
+    server.patch("/messages/:id", function (req: Request, res: Response) {
+        const messageId = parseInt(req.params.id, 10);
+        const { text, username } = req.body;
+    
+        const messageIndex = messages.findIndex(message => message.id === messageId);
+        
+        if (messageIndex === -1) {
+            res.status(404).send({
+                message: "Message not found",
+            });
+            return;
+        }
+    
+        if (messages[messageIndex].username.trim().replaceAll(" ", "") !== username.trim().replaceAll(" ", ""))  {
+            res.status(403).send({
+                message: "You can only edit your own messages",
+            });
+            return;
+        }
+    
+        if (typeof text !== "string") {
+            res.status(400).send({
+                message: "Message text must be a string",
+            });
+            return;
+        }
+    
+        if (text.length < 1) {
+            res.status(400).send({
+                message: "Message text must be at least 1 character long",
+            });
+            return;
+        }
+    
+        if (text.length > 500) {
+            res.status(400).send({
+                message: "Message text must be no more than 500 characters long",
+            });
+            return;
+        }
+    
+        messages[messageIndex].text = text;
+        res.status(200).send(messages[messageIndex]);
+    });
+    
+    server.delete("/messages/:id", function (req: Request, res: Response) {
+        const messageId = parseInt(req.params.id, 10);
+        const { username } = req.body;
+    
+        const messageIndex = messages.findIndex(message => message.id === messageId);
+    
+        if (messageIndex === -1) {
+            res.status(404).send({
+                message: "Message not found",
+            });
+            return;
+        }
+    
+        if (messages[messageIndex].username.trim().replaceAll(" ", "") !== username.trim().replaceAll(" ", "")) {
+            res.status(403).send({
+                message: "You can only delete your own messages",
+            });
+            return;
+        }
+    
+        messages.splice(messageIndex, 1);
+        res.status(200).send({
+            message: "Message deleted successfully",
+        });
+    });
+
+    try {
+        await client.connect();
+        const res = await client.query('SELECT $1::text as message', ['Hello world!']);
+        console.log(res.rows[0].message); // Hello world!
+    } catch (err) {
+        console.error('Failed to connect to the database:', err);
+        process.exit(1);
+    }
+
+    server.listen(PORT, function () {
+        console.log(`[server]: Server is running at http://localhost:${PORT}`);
+    });
+}
+
+process.on("exit", async function () {
+  await client.end();
 });
 
 
-server.listen(PORT, function () {
-    console.log(`[server]: Server is running at http://localhost:${PORT}`);
-});
+initServer();
